90d3f84cd71373ed5efb9e66f69553fa739ba746
Alex Arasawa <aarasawa@gmail.com>
2023-12-04 17:04:01 -0800

    Solved first half of trebuchet

--- Day 1/Trebuchet.py
+++ Day 1/Trebuchet.py
@@ -1,3 +1,12 @@
-file_input = "Trebuchet_input.txt"
+import re
 
-print(file_input)
+file = open('trebuchet_input.txt')
+sum = 0
+
+for input_line in file:
+    all_digits = re.findall(r"\d", input_line)
+    concat_digits = all_digits[0] + all_digits[-1]
+    sum = sum + int(concat_digits)
+
+final_txt = "The sum of all the calibration values is {}"
+print(final_txt.format(sum))
